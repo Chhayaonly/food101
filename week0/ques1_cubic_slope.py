ec2319cce97e6a7f0e279ea2c9116d1872e76037
Chhayaonly <chhayachampion@gmail.com>
2024-12-04 21:44:35 +0530

    Delete week0 directory

--- week0/ques1_cubic_slope.py
+++
@@ -1,7 +0,0 @@
-def slope_of_cubic() :
-    t=eval(input("Enter the coefficients of the cubic polynomial in the format (cubic,quadratic,linear,constant):"))
-    x= int(input('Enter value of x at which slope of polynomial is to be calculated:'))
-    Slope= int(t[0])*3*x*x + int(t[1])*2*x + int(t[2])
-    print(f"Slope of given cubic polynomial at x={x} is {Slope}")
-
-slope_of_cubic()
