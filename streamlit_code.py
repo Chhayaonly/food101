a94e1c48a006e2c638848b57c50c024c995d2b63
Chhayaonly <chhayachampion@gmail.com>
2025-01-11 22:58:18 +0530

    streamlit_code.py

--- streamlit_code.py
+++ streamlit_code.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 from PIL import Image
 
 # Load the pre-trained model
-model = tf.keras.models.load_model('my food101model.h5')
+model = tf.keras.models.load_model('my_food101model.h5')
 
 # Function to preprocess the uploaded image
 def preprocess_image(image_file):
@@ -13,10 +13,9 @@ def preprocess_image(image_file):
     img_array = np.array(img) / 255.0  # Normalize pixel values
     img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
 
-    # Predict the class
     pred = model.predict(img_array)
-    pred_class = np.argmax(pred, axis=-1)[0]  # Get the class index with the highest probability
-    pred_confidence = np.max(pred)  # Confidence score for the predicted class
+    pred_class = np.argmax(pred, axis=-1)[0]
+    pred_confidence = np.max(pred)
 
     food_classes = [
         'burger', 'butter naan', 'chai', 'chapati', 'chole_bhature', 'dal makhani',
@@ -29,13 +28,15 @@ def preprocess_image(image_file):
 # Streamlit App
 st.title('BCS Food101 Classifier')
 
-uploaded_image = st.file_uploader("Upload an image of a food item ", type=["jpg", "jpeg", "png"])
+uploaded_image = st.file_uploader("Upload an image of a food item", type=["jpg", "jpeg", "png"])
 
 if uploaded_image:
     image = Image.open(uploaded_image)
     st.image(image, caption='Uploaded Image', width=224)
     st.write("")
     
-st.subheader('Prediction:')
-confidence, predicted_class = preprocess_image(uploaded_image)
-st.write(f'I am {confidence * 100:.2f}% confident that this is an image of **{predicted_class}**.')
+    st.subheader('Prediction:')
+    confidence, predicted_class = preprocess_image(uploaded_image)
+    st.write(f'I am {confidence * 100:.2f}% confident that this is an image of **{predicted_class}**.')
+else:
+    st.write("Please upload an image file.")
