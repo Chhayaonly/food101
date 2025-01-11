222d1ef942e19dbbd0184f9f4b224804a281d820
Chhayaonly <chhayachampion@gmail.com>
2025-01-11 22:00:02 +0530

    streamlit_code.py

--- streamlit_code.py
+++ streamlit_code.py
@@ -1,24 +1,18 @@
 import streamlit as st
 from tensorflow.keras.models import load_model
 import numpy as np
-import matplotlib.pyplot as plt
-import cv2
 
 # Load the pre-trained model
 model = tf.keras.models.load_model('my food101model.h5')
 
-
-def predict_food101(img_path):
-    # Read and preprocess the image
-    img = cv2.imread(img_path)
-    img = cv2.resize(img, (224, 224))  # Resize image to match model's input shape
-    img = img / 255.0  # Normalize the image (since ImageDataGenerator used rescale=1./255)
-
-    # Add a batch dimension
-    img = np.expand_dims(img, axis=0)
+# Function to preprocess the uploaded image
+def preprocess_image(image_file):
+    img = Image.open(image_file).resize((224, 224)).convert('RGB')  # Ensure RGB and resize
+    img_array = np.array(img) / 255.0  # Normalize pixel values
+    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
 
     # Predict the class
-    pred = model.predict(img)
+    pred = model.predict(img_array)
     pred_class = np.argmax(pred, axis=-1)[0]  # Get the class index with the highest probability
     pred_confidence = np.max(pred)  # Confidence score for the predicted class
 
@@ -30,23 +24,16 @@ def predict_food101(img_path):
 
     return pred_confidence, food_classes[pred_class]
 
-def show_rgb(image_path):
-    image = cv2.imread(image_path)
-    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    st.image(image_rgb, channels="RGB")
+# Streamlit App
+st.title('BCS Food101 Classifier')
 
-# Streamlit interface
-st.title('Food101 Image Classification')
+uploaded_image = st.file_uploader("Upload an image of a food item ", type=["jpg", "jpeg", "png"])
 
-uploaded_file = st.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png'])
-
-if uploaded_file is not None:
-    with open('temp_image.jpg', 'wb') as f:
-        f.write(uploaded_file.getbuffer())
-
-    st.subheader('Uploaded Image:')
-    show_rgb('temp_image.jpg')
-
-    st.subheader('Prediction:')
-    confidence, predicted_class = predict_food101('temp_image.jpg')
-    st.write(f'I am {confidence * 100:.2f}% confident that this is an image of **{predicted_class}**.')
+if uploaded_image:
+    image = Image.open(uploaded_image)
+    st.image(image, caption='Uploaded Image', width=224)
+    st.write("")
+    
+st.subheader('Prediction:')
+confidence, predicted_class = predict_food101('temp_image.jpg')
+st.write(f'I am {confidence * 100:.2f}% confident that this is an image of **{predicted_class}**.')
